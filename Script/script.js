ab137ae0390ae21b97748b7e299201cc34acf276
fsokoloff <fsokoloff@gmail.com>
2022-08-14 05:40:27 -0300

    Modificacion del tiempo de ejecucion del video

--- Script/script.js
+++ Script/script.js
@@ -1,8 +1,10 @@
 /*------VIDEO------*/
-
 let video = document.querySelector('video');
 let playBoton = document.querySelector("#play");
 let pauseBoton = document.querySelector("#pause");
+let duracion = document.querySelector("#duracion");
+let duracionTotal = document.querySelector("#duracionTotal");
+video.ontimeupdate = tiempos;
 
 playBoton.addEventListener('click', ()=>{
     video.play()
@@ -12,3 +14,7 @@ pauseBoton.addEventListener('click', ()=>{
     video.pause()
 });
 
+function tiempos() {
+    duracionTotal.innerHTML = `${Math.floor(video.duration / 60)}:${Math.floor(video.duration % 60)}`;
+    duracion.innerHTML = `${Math.floor(video.currentTime / 60)}:${Math.floor(video.currentTime % 60)}`;
+    };
